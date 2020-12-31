2e04bfc2d5f266b7ee7aeee7e9ce349cdfa90424
vas0x59 <vasily.yuryev.a1@gmail.com>
2020-12-31 14:22:51 +0700

    Create test sketch

---
+++ test.ino
@@ -0,0 +1,36 @@
+#include<A4988_stepper.h>
+
+
+A4988_Stepper::A4988_Stepper stepper(A4988_Stepper::NEMA_17_STEPS_PER_TURN, 3, 2, 4, 5, 7);
+
+void setup() {
+    stepper.setMode(A4988_Stepper::POSE);
+    stepper.setMicroStep(A4988_Stepper::SIXTEENTH_STEP);
+    // stepper.setTargetSpeedDegrees(4);
+    stepper.setStdSpeedDegrees(90);
+    Serial.begin(115200);
+    Serial.print(stepper.getTargetSpeedSteps());
+    stepper.setTargetPoseDegrees(180);
+    while(!stepper.targetPoseReached()) { stepper.tick();};
+    delay(500);
+    stepper.setTargetPoseDegrees(360);
+    while(!stepper.targetPoseReached()) { stepper.tick();};
+    // delay(1000);
+    delay(500);
+    stepper.setTargetPoseDegrees(-180);
+    while(!stepper.targetPoseReached()) { stepper.tick();};
+    // stepper.setTargetPoseDegrees(180);
+    stepper.setMode(A4988_Stepper::SPEED);
+    stepper.setTargetSpeedDegrees(90);
+}
+
+void loop() {
+    stepper.tick();
+    
+    // delay(1000);
+    // Serial.print("target speed: ");
+    // Serial.print(stepper.getTargetSpeedSteps());
+    // Serial.print("c speed: ");
+    // Serial.print(stepper.getSpeedSteps());
+    // Serial.println();
+}
